bd9e6ae3cb5cd27d63e031639cdc97e778008600
Zi-on <zioniflores12@yahoo.com>
2021-07-13 12:56:58 -0500

    added my dependencys and created server.js folder that runs a http://localhost:3000

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+
+app.listen(PORT, () => {
+    console.log(`listening on: http://localhost:${PORT}`);
+})
