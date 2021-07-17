93c727d493c366031d9ff8b5c78bf5a31959dc07
Zi-on <zioniflores12@yahoo.com>
2021-07-17 18:44:45 -0500

    finally got my delete request to work. added a delete note funtion that grabes every note compares it to the note id and deletes it from the db.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,17 @@
-const db = require('../db/db.json');
+const dataBase = require('../db/db.json');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '../db/db.json')))
     app.get('/api/notes', (req, res) => res.json(db));
-    app.get('./api/notes/:id', (req, res) => {
-
+    app.get('/api/notes/:id', (req, res) => {
      res.json(notes[req.params.id]);
      res.json(true);
     });
 
-    fs.readFile("db/db.json", "utf8", (err, data) => {
+    fs.readFile("db/db.json", (err, data) => {
+        if(err) throw err;
         notes = JSON.parse(data);
         console.log(notes);
     });
@@ -27,15 +27,20 @@ module.exports = (app) => {
       res.json(newNote)
     });
 
-    app.delete('./api/notes/:id', (req, res) => {
-        noteId = req.params.id.toString();
-        console.log(noteId);
-        data = JSON.parse(fs.readFile("db/db.json", "utf8"));
-
-        const newData = data.filter( notes => notes.id.toString() !== noteId);
-
-        fs.watchFile('.db/db.json', JSON.stringify(newData, null));
-
-        res.JSON(newData);
+    app.delete('/api/notes/:id', (req, res) => {
+       deleteNote(req.params.id, dataBase);
+       res.json(true);
+       console.log(req.params.id)
     });
+
+    function deleteNote(id, notesArray) {
+        for (let i = 0; i < notesArray.length; i++) {
+            let note = notesArray[i];
+
+            if (note.id == id) {
+                notesArray.splice(i, 1);
+                fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notesArray, null, 2))
+            }
+        }
+    }
 }
