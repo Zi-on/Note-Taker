49c7ffe61128bd0ffa5cf24b1ed9e399274baf62
Zi-on <zioniflores12@yahoo.com>
2021-07-17 20:02:07 -0500

    changed my port variable

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+
 
 
 app.use(express.static(path.join(__dirname, '../public')));
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-// app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
 require('./routes/apiRoutes')(app);
 
 
-
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`listening on: http://localhost:${PORT}`);
 });
