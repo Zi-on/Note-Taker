e87e17a7e2e414d3620bd912523d433ea685db9b
Zi-on <zioniflores12@yahoo.com>
2021-07-17 20:49:46 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Note-Taker
+# Note Taker
+    
+## Description 
+In this application you are able to add notes that save to the application
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [License](#license)
+- [Deployed-Link](#deployed-link)
+- [Contact Me](#contact-me)
+
+<img src="public/pictures/note-taker.png">
+
+## Installation
+1. after cloning the repo and opening it in you vs code your gonna wanna run npm install to install dependencies
+2. then run npm watch to start the server
+3. after that go to localhost:3001
+
+## Usage
+enter notes and delete them when youre finished
+
+## Contribution
+Zion Flores
+
+## Tests
+no tests
+
+## License
+MIT
+
+## Deployed Link
+https://peaceful-anchorage-35646.herokuapp.com/
+## Contact Me
+Linkedin: https://www.linkedin.com/in/zion-flores-684354214/
+
+GitHub: https://github.com/Zi-on
+
+Email: Zioniflores12@yahoo.com
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,46 +1,51 @@
-const dataBase = require('../db/db.json');
-const path = require('path');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const dataBase = require("../db/db.json");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '..', 'db/db.json')));
-    app.get('/api/notes', (req, res) => res.json(db));
-    app.get('/api/notes/:id', (req, res) => {
-     res.json(notes[req.params.id]);
-     res.json(true);
+  app.get("/api/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "..", "db/db.json"))
+  );
+  app.get("/api/notes", (req, res) => res.json(db));
+  app.get("/api/notes/:id", (req, res) => {
+    res.json(notes[req.params.id]);
+    res.json(true);
+  });
+
+  fs.readFile("db/db.json", (err, data) => {
+    if (err) throw err;
+    notes = JSON.parse(data);
+    console.log(notes);
+  });
+
+  app.post("/api/notes", (req, res) => {
+    newNote = req.body;
+
+    newNote.id = uuidv4();
+    notes.push(newNote);
+    fs.writeFile("db/db.json", JSON.stringify(notes, "/n"), (err) => {
+      if (err) throw err;
     });
+    res.json(newNote);
+  });
 
-    fs.readFile("db/db.json", (err, data) => {
-        if(err) throw err;
-        notes = JSON.parse(data);
-        console.log(notes);
-    });
-  
-    app.post('/api/notes', (req, res) => {
-      newNote = req.body;
+  app.delete("/api/notes/:id", (req, res) => {
+    deleteNote(req.params.id, dataBase);
+    res.json(true);
+    console.log(req.params.id);
+  });
 
-      newNote.id = uuidv4();
-      notes.push(newNote);
-      fs.writeFile("db/db.json",JSON.stringify(notes,'/n'),err => {
-          if (err) throw err;
-      });
-      res.json(newNote)
-    });
+  function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+      let note = notesArray[i];
 
-    app.delete('/api/notes/:id', (req, res) => {
-       deleteNote(req.params.id, dataBase);
-       res.json(true);
-       console.log(req.params.id)
-    });
-
-    function deleteNote(id, notesArray) {
-        for (let i = 0; i < notesArray.length; i++) {
-            let note = notesArray[i];
-
-            if (note.id == id) {
-                notesArray.splice(i, 1);
-                fs.writeFileSync(path.join(__dirname, '..', 'db/db.json'), JSON.stringify(notesArray, null, 2))
-            }
-        }
+      if (note.id == id) {
+        notesArray.splice(i, 1);
+        fs.writeFileSync(
+          path.join(__dirname, "..", "db/db.json"),
+          JSON.stringify(notesArray, null, 2)
+        );
+      }
     }
-}
+  }
+};
