5c1544d1d9a28739dce140b48f8ca173f7449a8b
Zi-on <zioniflores12@yahoo.com>
2021-07-17 19:51:08 -0500

    working on debugging heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 // app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 // app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
